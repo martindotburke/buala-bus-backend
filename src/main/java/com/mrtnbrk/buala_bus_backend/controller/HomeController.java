780101dd67ea7ee6ca6d7949453a2ddcd3719c97
Martin Burke <martin.burke@roxorgaming.com>
2024-11-07 15:08:02 +0000

    Return test data as JSON

--- src/main/java/com/mrtnbrk/buala_bus_backend/controller/HomeController.java
+++ src/main/java/com/mrtnbrk/buala_bus_backend/controller/HomeController.java
@@ -1,13 +1,15 @@
 package com.mrtnbrk.buala_bus_backend.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+   @CrossOrigin
    @RequestMapping("/")
    public String home() {
-      return "Buala Bus!";
+      return "{ data: 'Buala Bus!' }";
    }
 }
